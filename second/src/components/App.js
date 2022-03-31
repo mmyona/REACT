c6c860d141a0e62015c6cc6c1e3bff021224a5d1
mmyona <hyon2001@ajou.ac.kr>
2022-03-31 15:38:53 +0900

    삭제, 정렬 기능 구현

--- second/src/components/App.js
+++ second/src/components/App.js
@@ -1,10 +1,32 @@
 import ReviewList from "./ReviewList";
-import items from "../mock.json";
+import mockItems from "../mock.json";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState(mockItems);
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+  /*
+ - sort 메서드를 이용한 정렬
+작은 순서대로 정렬: numbers.sort((a, b) => a - b)
+크기가 큰 순서대로 정렬: numbers.sort((a, b) => b - a)
+  */
+
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleBestClick = () => setOrder("rating");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
   return (
     <div>
-      <ReviewList items={items} />
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>평점순</button>
+      </div>
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
