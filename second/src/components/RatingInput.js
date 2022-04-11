221da07e599fae0e36ca6900186f835d2a467259
mmyona <hyon2001@ajou.ac.kr>
2022-04-11 23:09:59 +0900

    rating input

---
+++ second/src/components/RatingInput.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Rating from "./Rating";
+import "../css/RatingInput.css";
+
+function RatingInput({ name, value, onChange }) {
+  const [rating, setRating] = useState(value);
+
+  const handleSelect = (nexValue) => onChange(name, nextValue);
+
+  const handleMouesOut = () => setRating(value);
+
+  return (
+    <Rating
+      value={rating}
+      onSelect={handleSelect}
+      onHover={setRating}
+      onMouseOut={handleMouesOut}
+    />
+  );
+}
+
+export default RatingInput;
